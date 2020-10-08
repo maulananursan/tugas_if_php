c06494a2f3d7ccfd55690333e14e8eae7246f40e
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-10-08 11:31:50 +0700

    Add files via upload

---
+++ tugas_if_php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	$a=50;
+	 
+	 if ($a % 2 == 0) {
+	 	echo $a, " adalah angka genap";
+	 }else{
+	 	echo $a, " adalah angka ganjil";
+	 }
+	 
+	?>
+</body>
+</html>
